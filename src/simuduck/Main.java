84b8eae006a3a69412f248106bfa1ee2c2ee7447
Kim DongJin <kxv1234@naver.com>
2020-03-23 18:01:26 +0900

    SimUDuck 에제 인터페이스와 추상클래스 활용

---
+++ src/simuduck/Main.java
@@ -0,0 +1,11 @@
+package simuduck;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Duck mallard = new MallardDuck();
+		mallard.performFly();
+		mallard.performQuack();
+	}
+
+}
